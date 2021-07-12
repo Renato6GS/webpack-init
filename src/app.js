ba721ceb7a538205f21d20f8c3b66cfc649eebd4
Renatogranados <58044053+Renatogranados@users.noreply.github.com>
2021-07-12 20:17:34 -0300

    feat: project with basic requirements

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// You can add a list of files
+function importAll(r) {
+   return r.keys().map(r);
+}
+importAll(require.context('./static/img', false, /\.(png|jpe?g|svg)$/));
+
+// ... or, add files individually
+import './vendors/normalize/normalize.css';
+import './styles/main.scss';
---
+++ webpack/webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const cssRules = {
+   test: /\.(sa|sc|c)ss$/,
+   use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+};
+
+const imageRules = {
+   test: /\.(jpg|png|gif|jpeg|svg)$/,
+   use: {
+      loader: 'file-loader',
+      options: {
+         name: '[name].[ext]',
+         outputPath: (url, resourcePath, context) => {
+            // `resourcePath` is original absolute path to asset
+            // `context` is directory where stored asset (`rootContext`) or `context` option
+            // To get relative path you can use
+            const relativePath = path.relative(context, resourcePath);
+
+            if (`src${path.sep}static${path.sep}img${path.sep}desktop${path.sep}${url}` === relativePath) {
+              return `static/img/desktop/${url}`;
+            }
+
+            return `static/img/${url}`;
+          },
+         useRelativePath: true,
+      },
+   },
+};
+
+const handleBarsRules = {
+   test: /\.handlebars/,
+   loader: 'handlebars-loader',
+};
+
+const imageMinifyRules = {
+   loader: 'image-webpack-loader',
+   options: {
+      mozjpeg: {
+         progressive: true,
+      },
+      // optipng.enabled: false will disable optipng
+      optipng: {
+         enabled: true,
+      },
+      pngquant: {
+         quality: [0.8, 0.9],
+         speed: 4,
+      },
+      gifsicle: {
+         interlaced: false,
+      },
+      // the webp option will enable WEBP
+      webp: {
+         quality: 75,
+      },
+   },
+};
+
+module.exports = {
+   entry: './src/app.js',
+   output: {
+      path: path.resolve(__dirname, '../dist'),
+      filename: 'js/bundle.js',
+   },
+   module: {
+      rules: [cssRules, imageRules, handleBarsRules, imageMinifyRules],
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: './src/index.handlebars',
+         minify: {
+            collapseWhitespace: true,
+            keepClosingSlash: true,
+            removeComments: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            useShortDoctype: true,
+         },
+      }),
+      new MiniCssExtractPlugin({
+         filename: 'css/[name]-styles.css',
+      }),
+   ],
+};
